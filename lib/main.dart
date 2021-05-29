9261d248fcdee176911cf34f31ea8ee6254e5260
miragessee <miragessee@hotmail.com>
2021-05-29 21:07:25 +0300

    Dark Theme Eklendi, PopupMenuItem Listesinde tireler (---) yerine Divider kullanıldı, İller sayfası eklendi (PageView)

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Barış ARSLAN Tez',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      // ),
+      theme: ThemeData.dark(),
       home: MyHomePage(title: 'Barış ARSLAN Tez'),
     );
   }
@@ -33,6 +34,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   WhyFarther? _selection;
 
+  // create a PageController
+  final _controller = PageController();
+  bool _shouldAnimate = true; // whether we animate or jump
+
+  final ButtonStyle style =
+  ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
   @override
   Widget build(BuildContext context) {
     double orjWidth = MediaQuery.of(context).size.width;
@@ -47,9 +55,11 @@ class _MyHomePageState extends State<MyHomePage> {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-          title: Center(child: const Text('Küçük ve Orta Boy İşletmeler İçin ERP Sistemi')),
+          title: Center(
+              child:
+                  const Text('Küçük ve Orta Boy İşletmeler İçin ERP Sistemi')),
           bottom: TabBar(
-            indicatorColor: Colors.blue,
+            indicatorColor: Colors.transparent,
             unselectedLabelColor: Colors.white,
             automaticIndicatorColorAdjustment: false,
             tabs: <Widget>[
@@ -68,136 +78,171 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
             onTap: (index) {
               print(index);
-              
+
               if (index == 0) {
-                showMenu(context: context, position: RelativeRect.fromSize(Rect.fromLTRB(0, 100, 0, 0), Size.infinite), items: <PopupMenuEntry<WhyFarther>>[
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.harder,
-                    child: Text('İller'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.smarter,
-                    child: Text('İlçeler'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('--------------'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.tradingCharter,
-                    child: Text('Ürünler'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('--------------'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Müşteri'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Müşteri Adres'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('--------------'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Tedarikçiler'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Tedarikçi Adres'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('--------------'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Müşteri Mali Bilgileri'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Tedarikçi Mali Bilgileri'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('--------------'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Ölçekler'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Durumlar'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('--------------'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Çıkış'),
-                  ),
-                ]);
+                showMenu(
+                    context: context,
+                    position: RelativeRect.fromSize(
+                        Rect.fromLTRB(0, 100, 0, 0), Size.infinite),
+                    items: <PopupMenuEntry<WhyFarther>>[
+                      PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.harder,
+                        child: ListTile(
+                          title: Text("İller"),
+                          onTap: () {
+                            print('iller tikladin');
+                            Navigator.pop(context);
+                            _controller.animateToPage(1,
+                                duration: Duration(seconds: 1),
+                                curve: Curves.easeOut);
+                          },
+                        ),
+                      ),
+                      PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.harder,
+                        child: ListTile(
+                          title: Text("İlçeler"),
+                          onTap: () {
+                            print('ilçeler tikladin');
+                            Navigator.pop(context);
+                            _controller.animateToPage(2,
+                                duration: Duration(seconds: 1),
+                                curve: Curves.easeOut);
+                          },
+                        ),
+                      ),
+                      PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.harder,
+                        child: const Divider(
+                          height: 10,
+                          thickness: 5,
+                          indent: 0,
+                          endIndent: 0,
+                        ),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.tradingCharter,
+                        child: Text('Ürünler'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('--------------'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Müşteri'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Müşteri Adres'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('--------------'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Tedarikçiler'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Tedarikçi Adres'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('--------------'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Müşteri Mali Bilgileri'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Tedarikçi Mali Bilgileri'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('--------------'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Ölçekler'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Durumlar'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('--------------'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Çıkış'),
+                      ),
+                    ]);
               } else if (index == 1) {
-                showMenu(context: context, position: RelativeRect.fromSize(Rect.fromLTRB(tabWidth, 100, 0, 0), Size.infinite), items: <PopupMenuEntry<WhyFarther>>[
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.harder,
-                    child: Text('Stok Girişi'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.smarter,
-                    child: Text('Stok Çıkışı'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('--------------'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.tradingCharter,
-                    child: Text('Satınalmalar'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Satışlar'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('--------------'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Tahsilatlar'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Ödemeler'),
-                  ),
-                ]);
+                showMenu(
+                    context: context,
+                    position: RelativeRect.fromSize(
+                        Rect.fromLTRB(tabWidth, 100, 0, 0), Size.infinite),
+                    items: <PopupMenuEntry<WhyFarther>>[
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.harder,
+                        child: Text('Stok Girişi'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.smarter,
+                        child: Text('Stok Çıkışı'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('--------------'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.tradingCharter,
+                        child: Text('Satınalmalar'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Satışlar'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('--------------'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Tahsilatlar'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Ödemeler'),
+                      ),
+                    ]);
               } else if (index == 2) {
-                showMenu(context: context, position: RelativeRect.fromSize(Rect.fromLTRB(tabWidth*2, 100, 0, 0), Size.infinite), items: <PopupMenuEntry<WhyFarther>>[
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.harder,
-                    child: Text('Ürün Sorgulama'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.smarter,
-                    child: Text('Stok Giriş Raporu'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.selfStarter,
-                    child: Text('Stok Çıkış Raporu'),
-                  ),
-                  const PopupMenuItem<WhyFarther>(
-                    value: WhyFarther.tradingCharter,
-                    child: Text('Satınalma-Ödemeler'),
-                  ),
-                ]);
+                showMenu(
+                    context: context,
+                    position: RelativeRect.fromSize(
+                        Rect.fromLTRB(tabWidth * 2, 100, 0, 0), Size.infinite),
+                    items: <PopupMenuEntry<WhyFarther>>[
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.harder,
+                        child: Text('Ürün Sorgulama'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.smarter,
+                        child: Text('Stok Giriş Raporu'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.selfStarter,
+                        child: Text('Stok Çıkış Raporu'),
+                      ),
+                      const PopupMenuItem<WhyFarther>(
+                        value: WhyFarther.tradingCharter,
+                        child: Text('Satınalma-Ödemeler'),
+                      ),
+                    ]);
               }
             },
           ),
@@ -228,6 +273,113 @@ class _MyHomePageState extends State<MyHomePage> {
           //     ],
           //   )
           // ],
+        ),
+        // body: TabBarView(
+        //   children: [
+        //     Icon(Icons.directions_car),
+        //     Icon(Icons.directions_transit),
+        //     Icon(Icons.directions_bike),
+        //   ],
+        // ),
+        body: PageView(
+          controller: _controller, // assign it to PageView
+          children: <Widget>[
+            Text(''), // iller ilk sayfasi, boş
+            Card(
+              margin: const EdgeInsets.only(
+                  left: 20.0, right: 20.0, top: 20, bottom: 20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    title: Center(
+                        child: Text('İl Tanımlama Ekranı',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .copyWith(fontWeight: FontWeight.bold))),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Flexible(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.only(
+                              left: 20.0, right: 20.0),
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                readOnly: false,
+                                decoration: const InputDecoration(
+                                  labelText: 'İl Kodu:',
+                                ),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Lütfen İl Kodu giriniz';
+                                  }
+                                  return null;
+                                },
+                              ),
+                              const SizedBox(height: 16),
+                              TextFormField(
+                                readOnly: false,
+                                decoration: const InputDecoration(
+                                  labelText: 'İl Adı:',
+                                ),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Lütfen İl Adı giriniz';
+                                  }
+                                  return null;
+                                },
+                              ),
+                              const SizedBox(height: 16),
+                              ElevatedButton(
+                                style: style,
+                                onPressed: () {},
+                                child: const Text('Kaydet'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ), // iller dolu sayfasi
+            Card(
+              margin: const EdgeInsets.only(
+                  left: 20.0, right: 20.0, top: 20, bottom: 20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const ListTile(
+                    leading: Icon(Icons.album),
+                    title: Text('dumancore'),
+                    subtitle:
+                        Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      TextButton(
+                        child: const Text('BUY TICKETS'),
+                        onPressed: () {/* ... */},
+                      ),
+                      const SizedBox(width: 8),
+                      TextButton(
+                        child: const Text('LISTEN'),
+                        onPressed: () {/* ... */},
+                      ),
+                      const SizedBox(width: 8),
+                    ],
+                  ),
+                ],
+              ),
+            ), // page2
+          ],
         ),
       ),
     );
